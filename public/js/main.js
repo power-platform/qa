5b2237e525aa2e69d8dfab3ff87ce226c2c239f0
Aagat Adhikari <hi@aagat.com>
2016-07-16 21:12:11 +0545

    nicer experience with expanding form

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,13 @@
 $(function() {
+	var char_counter = 0;
 	$( "#homepage-question-form" ).keypress(function() {
+		char_counter++;
 		$(this).attr("rows", "10");
-		$(this).focusout(function() {
-			$(this).attr("rows", 1);
-		});
+		if (char_counter < 20) {
+			$(this).focusout(function() {
+				$(this).attr("rows", 1);
+			});
+			char_counter = 0;
+		}
 	});
 });
